d8e54427d6dc550c4b53d2d095b76016ad3c74a3
Mateo Garcia <mateog@stanford.edu>
2017-08-01 15:29:59 -0700

    Error 'attempt to shift right with overflow' on end_idx passed as multiple of 8 to set_bits.

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,26 @@ fn main() {
     println!("Hello, world!");
 
     let mut bitmap = bitmap::Bitmap::new();
-
     println!("{}", bitmap);
 
     let mut no_overlap = bitmap.set_bits(1, 3);
-
     println!("{}", bitmap);
 
     assert!(no_overlap);
 
     bitmap.set_bit(5);
-
     println!("{}", bitmap);
 
     bitmap.clear_bit(2);
-
     println!("{}", bitmap);
 
     no_overlap = bitmap.set_bits(1, 6);
+    println!("{}", bitmap);
 
     assert!(!no_overlap);
+
+    no_overlap = bitmap.set_bits(6, 8);
+    println!("{}", bitmap);
+
+    assert!(no_overlap);
 }
